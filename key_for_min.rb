5b0efa711ea0ae7b22d4f9266788162cf1e47c12
Janan Akkad <jananakkad@hotmail.co.uk>
2022-04-20 11:51:21 +0100

    Completed assignment

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,13 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+    min_key = nil
+    min_value = nil
+    name_hash.each {|key_name, value_number|
+        if min_value == nil || value_number < min_value
+            min_value = value_number
+            min_key = key_name
+        end
+    }
+    min_key
 end
